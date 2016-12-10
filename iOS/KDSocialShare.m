75c0936ef824dfdf1d1214015d5ec37badf192ee
Cao Junquan <i@cdog.me>
2016-12-10 21:46:55 +0800

    Add support for Sina Weibo

--- iOS/KDSocialShare.m
+++ iOS/KDSocialShare.m
@@ -98,6 +98,45 @@ RCT_EXPORT_METHOD(shareOnFacebook:(NSDictionary *)options
   [ctrl presentViewController:composeCtl animated:YES completion: nil];
 }
 
+RCT_EXPORT_METHOD(shareOnSinaWeibo:(NSDictionary *)options
+                  callback: (RCTResponseSenderBlock)callback)
+{
+  NSString *serviceType = SLServiceTypeSinaWeibo;
+  SLComposeViewController *composeCtl = [SLComposeViewController composeViewControllerForServiceType:serviceType];
+  
+  if ([options objectForKey:@"link"] && [options objectForKey:@"link"] != [NSNull null]) {
+    NSString *link = [RCTConvert NSString:options[@"link"]];
+    [composeCtl addURL:[NSURL URLWithString:link]];
+  }
+  
+  if ([options objectForKey:@"image"] && [options objectForKey:@"image"] != [NSNull null]) {
+    [composeCtl addImage: [UIImage imageNamed: options[@"image"]]];
+  } else if ([options objectForKey:@"imagelink"] && [options objectForKey:@"imagelink"] != [NSNull null]) {
+    NSString *imagelink = [RCTConvert NSString:options[@"imagelink"]];
+    UIImage *image = [UIImage imageWithData:[NSData dataWithContentsOfURL:[NSURL URLWithString:imagelink]]];
+    [composeCtl addImage:image];
+  }
+  
+  if ([options objectForKey:@"text"] && [options objectForKey:@"text"] != [NSNull null]) {
+    NSString *text = [RCTConvert NSString:options[@"text"]];
+    [composeCtl setInitialText:text];
+  }
+  
+  [composeCtl setCompletionHandler:^(SLComposeViewControllerResult result) {
+    if (result == SLComposeViewControllerResultDone) {
+      // Sent
+      callback(@[@"success"]);
+    }
+    else if (result == SLComposeViewControllerResultCancelled){
+      // Cancelled
+      callback(@[@"cancelled"]);
+    }
+  }];
+  
+  UIViewController *ctrl = [[[[UIApplication sharedApplication] delegate] window] rootViewController];
+  [ctrl presentViewController:composeCtl animated:YES completion: nil];
+}
+
 
 
 @end
